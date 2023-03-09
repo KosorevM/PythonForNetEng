69a80f9408f8cc775fea8f83fcc76752b8d5cd98
KosorevM <sokolov-mickel@mail.ru>
2023-03-10 00:14:33 +0300

    add completed task_9_2a.py

--- exercises/09_functions/task_9_2a.py
+++ exercises/09_functions/task_9_2a.py
@@ -47,3 +47,19 @@ trunk_config = {
     "FastEthernet0/2": [11, 30],
     "FastEthernet0/4": [17],
 }
+
+
+def generate_trunk_config(intf_vlan_mapping, trunk_template):
+    config = {}
+    for int in intf_vlan_mapping:
+        config_temp = []
+        for command in trunk_template:
+            if command.endswith('allowed vlan'):
+                vlans = str(intf_vlan_mapping[int]).strip('[').strip(']').replace(' ', '')
+                config_temp.append(f"{command} {vlans}")
+                continue
+            else:
+                config_temp.append(command)
+        config[int] = config_temp
+    return config
+generate_trunk_config(trunk_config, trunk_mode_template)

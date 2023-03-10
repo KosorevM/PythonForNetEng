389671a7febbc3c478d25d073716dca0bb25047b
KosorevM <sokolov-mickel@mail.ru>
2023-03-10 16:30:40 +0300

    add completed task_9_3a.py

--- exercises/09_functions/task_9_3a.py
+++ exercises/09_functions/task_9_3a.py
@@ -25,3 +25,25 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+def get_int_vlan_map(config_filename):
+    with open(config_filename) as f:
+        vlans_on_int = ({},{})
+        interface = ''
+        for line in f:
+            line_list = line.split()
+            if "interface" in line_list:
+                interface = line_list[1]
+            if "mode access" in line:
+                vlans_on_int[0][interface] = 1
+            if "access vlan" in line:
+                vlans_on_int[0][interface] = int(line_list[3])
+            if "trunk allowed" in line:
+                    a = line_list[4].split(',')
+                    c = []
+                    for b in a:
+                        b = int(b)
+                        c.append(b)
+                    vlans_on_int[1][interface] = c
+    return vlans_on_int
+
+print(get_int_vlan_map('config_sw2.txt'))

df351ddfd5f507a46711b5fe29593ec60f6759a7
KosorevM <sokolov-mickel@mail.ru>
2023-11-21 09:53:00 +0300

    add solution for task 12_1

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,16 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+list_of_addresses = ("10.100.9.254", "8.8.8.8")
+ip_address_dostupni = []
+ip_address_ne_dostupni = []
+def ping_ip_addresses(input_list):
+    for _ in input_list:
+        result = subprocess.run(['ping', '-c', '1', _], stdout=subprocess.DEVNULL)
+        ip_address_dostupni.append(_) if result.returncode == 0 else ip_address_ne_dostupni.append(_)
+    final_tuple = ip_address_dostupni, ip_address_ne_dostupni
+    return(final_tuple)
+
+if __name__ == "__main__":
+    print(ping_ip_addresses(list_of_addresses))

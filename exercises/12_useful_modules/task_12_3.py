aae3c3dfe50087afef2b69ea24d55b7b603fa756
KosorevM <sokolov-mickel@mail.ru>
2023-11-21 10:59:14 +0300

    add solution for task 12_3

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -18,3 +18,14 @@ Reachable    Unreachable
              10.1.1.9
 
 """
+import tabulate
+
+def print_ip_table(reachable, not_reachable):
+    my_tuple = {'Reachable' : reachable, 'Unreachable': not_reachable}
+    print(tabulate.tabulate(my_tuple, headers='keys'))
+
+list_reachable = ["10.10.1.7", "10.10.1.8", "10.10.1.9", "10.10.1.15"]
+list_not_reachable = ["10.1.1.7", "10.1.1.8"]
+
+if __name__ == "__main__":
+    print_ip_table(list_reachable, list_not_reachable)
